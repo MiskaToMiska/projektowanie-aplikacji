2bc9e5cb7bef0afeee2a332b6c58d15abd255d0f
Misek <75534732+MiskaToMiska@users.noreply.github.com>
2026-09-10 09:39:00 +0200

    Add new HTML and JavaScript files for dynamic content manipulation
    - Created pobierania2.html with basic structure and elements
    - Added pobierania2.js for future JavaScript functionality
    - Created pobrania.html with a list of items and IDs/classes for selection
    - Implemented pobrania.js to demonstrate DOM manipulation and dynamic element addition

---
+++ losowe/pobrania/pobrania.js
@@ -0,0 +1,15 @@
+let test4 = document.querySelector('#pierwszy')
+console.log(test4)
+
+let test5 = document.querySelector('')
+console.log(test4)
+
+let test6 = document.querySelector('ul li')
+console.log(test4)
+
+let ulist = document.querySelector('ul')
+console.log(ulist)
+
+let newLi = document.createElement('li')
+
+ulist.appendChild(newLi).textContent = 'element dodany dynamicznie'
