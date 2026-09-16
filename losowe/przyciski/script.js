13779c64aad875ba158bda95979b4394e80761d9
Misek <75534732+MiskaToMiska@users.noreply.github.com>
2026-09-16 07:57:30 +0200

    projektowanie aplikacji lekcja 7:10

---
+++ losowe/przyciski/script.js
@@ -0,0 +1,11 @@
+const btn1 = document.querySelector(".btn1");
+const btn2 = document.querySelector(".btn2");
+const p1 = document.querySelector(".p1");
+const p2 = document.querySelector(".p2");
+const square = document.querySelector(".square");
+
+const fun1 = () => {
+  console.log("czesc");
+};
+
+btn1.addEventListener("click", fun1);
